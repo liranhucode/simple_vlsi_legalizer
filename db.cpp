8af244c8daf106a8165b57865e2f4d2dd4882fcc
liranhucode <liranhucode@gmail.com>
2022-07-25 05:02:25 +0800

    place row 1

--- db.cpp
+++ db.cpp
@@ -3,13 +3,10 @@
 
 void DB::Init()
 {
+    Timer timer("Init DB");
     InitFixedCellToRow();
     CreateSubRow();
-}
-
-void DB::CreateSubRow()
-{
-
+    timer.Report();
 }
 
 void DB::InitFixedCellToRow()
@@ -33,8 +30,81 @@ void DB::InitFixedCellToRow()
             {
                 rows_[r].fixed_cells.emplace_back(cell);
             }
-            
         }
     }
     assert(n == num_fixed_cell_);
+}
+
+void DB::CreateSubRow()
+{
+    //std::vector<vector<SubRow>> subrows;
+    subrows_.resize(num_row_);
+
+    for (int i = 0; i < num_row_; i++)
+    {
+        if (rows_[i].fixed_cells.empty())
+        {
+            SubRow row;
+            row.x_coord = rows_[i].x_coord;
+            row.y_coord = rows_[i].y_coord;
+            subrows_[i].push_back(row);
+        } 
+        else 
+        {
+            SortFixedCell(i);
+
+            int start_x = rows_[i].x_coord;
+            for (int j = 0; j < rows_[i].fixed_cells.size(); ++j)
+            {
+                if (rows_[i].fixed_cells[j].init_x == start_x) 
+                {
+                    start_x = rows_[i].fixed_cells[j].init_x + rows_[i].fixed_cells[j].width;
+                }
+                else 
+                {
+                    SubRow row;
+                    row.x_coord = start_x;
+                    row.y_coord = rows_[i].y_coord;
+                    row.width = rows_[i].y_coord - start_x;
+                    subrows_[i].push_back(row);
+                }
+            }
+
+            unsigned last = rows_[i].fixed_cells.size() - 1;
+            if (rows_[i].fixed_cells[last].init_x + rows_[i].fixed_cells[last].width < rows_[i].x_coord + rows_[i].width)
+            {
+                SubRow row;
+                row.x_coord = rows_[i].fixed_cells[last].init_x + rows_[i].fixed_cells[last].width;
+                row.y_coord = rows_[i].y_coord;
+                row.width =  rows_[i].x_coord + rows_[i].width - row.x_coord;
+                subrows_[i].push_back(row);
+            }
+        }
+    }
+}
+
+void DB::SortFixedCell(int rid)
+{
+
+    if (rows_[rid].fixed_cells.size() == 1)
+    {
+        return ;
+    }
+
+    sort(rows_[rid].fixed_cells.begin(),rows_[rid].fixed_cells.end(), [] (const Cell &a, const Cell &b) -> bool {
+        return a.init_x < b.init_x;
+    });
+}
+
+int DB::FindClosestRowForCell(int id)
+{
+    int r = round(cells_[id].init_y / site_height_);
+    if (legal_option.is_debug)
+    {
+        std::cout << "cell: " << cells_[id].name << " ";
+        std::cout << "coord:" << cells_[id].init_x << " " << cells_[id].init_y << " ";
+        std::cout << "nearest row: " << r << std::endl;
+    }
+
+    return r;
 }
